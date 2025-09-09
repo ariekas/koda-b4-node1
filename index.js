9a378920ac56f34147a53b62b7f3a55e2cf7900c
ariekas <ariariekas@gmail.com>
2025-09-09 17:45:18 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const create = require("./lib/createMusic");
+const move = require("./lib/moveMusic");
+
+
+const music = [
+  "Rossa - Nada Nada Cinta",
+  "Rossa - Yang Terpilih",
+  "Rossa - Kembali",
+  "Rossa - Tegar",
+  "Rizky Febian - Jejak",
+  "Rizky Febian - Cuek",
+  "Rizky Febian - Kesempurnaan Cinta",
+  "Bulan Sutena - Ghosting",
+  "Bulan Sutena - Tetap Punyaku",
+  "Raavfy - Badut",
+];
+
+create.createFile(music);
+move.moveMusic(music);
